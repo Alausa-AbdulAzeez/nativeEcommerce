3b487698d1fbb8284e08927390c693e2dd0f98c3
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-08-25 14:51:08 +0100

    first version UI complete

--- screens/ProductDetails.jsx
+++ screens/ProductDetails.jsx
@@ -1,41 +1,41 @@
-import { View, Text, TouchableOpacity, Image } from "react-native";
-import React, { useState } from "react";
-import { styles } from "./productDetails.styles";
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
+import { styles } from './productDetails.styles'
 import {
   Fontisto,
   Ionicons,
   MaterialCommunityIcons,
   SimpleLineIcons,
-} from "@expo/vector-icons";
-import { COLORS } from "../constants/theme";
+} from '@expo/vector-icons'
+import { COLORS } from '../constants/theme'
 
 const ProductDetails = ({ navigation }) => {
   // PRODUCT COUNT
-  const [productCount, setProductCount] = useState(1);
+  const [productCount, setProductCount] = useState(1)
 
   // FUNCTION TO HANDLE PRODUCT INCREAMENT AND DECREMENT
   const handleProductCountChange = (changeType) => {
-    if (changeType === "increase") {
-      setProductCount((prev) => prev + 1);
+    if (changeType === 'increase') {
+      setProductCount((prev) => prev + 1)
     }
-    if (changeType === "decrease" && productCount > 1) {
-      setProductCount((prev) => prev - 1);
+    if (changeType === 'decrease' && productCount > 1) {
+      setProductCount((prev) => prev - 1)
     }
-  };
+  }
   // END OF FUNCTION TO HANDLE PRODUCT INCREAMENT AND DECREMENT
 
   return (
     <View style={styles.container}>
       <View style={styles.upperRow}>
         <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Ionicons name="chevron-back-circle" size={30} />
+          <Ionicons name='chevron-back-circle' size={30} />
         </TouchableOpacity>
         <TouchableOpacity onPress={() => {}}>
-          <Ionicons name="heart" size={30} color={COLORS.primary} />
+          <Ionicons name='heart' size={30} color={COLORS.primary} />
         </TouchableOpacity>
       </View>
       <Image
-        source={require("../assets/images/fn2.jpg")}
+        source={require('../assets/images/fn2.jpg')}
         style={styles.image}
       />
       <View style={styles.details}>
@@ -48,24 +48,22 @@ const ProductDetails = ({ navigation }) => {
         <View style={styles.ratingAndCountRow}>
           <View style={styles.rating}>
             {[1, 2, 3, 4, 5].map((index) => {
-              return (
-                <Ionicons name="star" color="gold" key={index} size={24} />
-              );
+              return <Ionicons name='star' color='gold' key={index} size={24} />
             })}
             <Text style={styles.ratingText}>(5.0)</Text>
           </View>
           <View style={styles.count}>
             <TouchableOpacity
-              onPress={() => handleProductCountChange("decrease")}
+              onPress={() => handleProductCountChange('decrease')}
             >
-              <SimpleLineIcons name="minus" size={20} />
+              <SimpleLineIcons name='minus' size={20} />
             </TouchableOpacity>
 
             <Text style={styles.countText}>{productCount}</Text>
             <TouchableOpacity
-              onPress={() => handleProductCountChange("increase")}
+              onPress={() => handleProductCountChange('increase')}
             >
-              <SimpleLineIcons name="plus" size={20} />
+              <SimpleLineIcons name='plus' size={20} />
             </TouchableOpacity>
           </View>
         </View>
@@ -82,11 +80,11 @@ const ProductDetails = ({ navigation }) => {
         </View>
         <View style={styles.locationAndDeliveryRow}>
           <View style={styles.location}>
-            <Ionicons name="location-outline" size={20} />
+            <Ionicons name='location-outline' size={20} />
             <Text style={styles.locationText}>Shanghai</Text>
           </View>
           <View style={styles.delivery}>
-            <MaterialCommunityIcons name="truck-delivery-outline" size={20} />
+            <MaterialCommunityIcons name='truck-delivery-outline' size={20} />
             <Text style={styles.deliveryText}> Free Delivery</Text>
           </View>
         </View>
@@ -95,13 +93,13 @@ const ProductDetails = ({ navigation }) => {
             <Text style={styles.buyNowText}>BUY NOW</Text>
           </TouchableOpacity>
 
-          <View style={styles.bag}>
-            <Fontisto name="shopping-bag" size={20} />
-          </View>
+          <TouchableOpacity style={styles.bag}>
+            <Fontisto name='shopping-bag' size={20} color={COLORS.white} />
+          </TouchableOpacity>
         </View>
       </View>
     </View>
-  );
-};
+  )
+}
 
-export default ProductDetails;
+export default ProductDetails
